46d51758ed839bef90109efe10debe975c600267
472420531@qq.com <472420531@qq.com>
2018-06-08 17:01:32 +0800

    springboot版本的机器人监控系统 完工交付版本

--- src/main/java/com/example/robotdemo/controller/ClientController.java
+++ src/main/java/com/example/robotdemo/controller/ClientController.java
@@ -26,39 +26,41 @@ public class ClientController {
     public @ResponseBody
     ReturnMsg start(HttpServletResponse response){
         response.addHeader("Access-Control-Allow-Origin","*");
-//        try {
-//            // 1.创建 socket 指定服务器地址和端
-//            Socket client = new Socket("127.0.0.1", 8001);
-//            // 2.客户端向服务器发送登录信息
-//            OutputStream os = client.getOutputStream();// 字节输出流
-//            PrintWriter pw = new PrintWriter(new OutputStreamWriter(os,"UTF-8"),true);
-//            pw.write("start");
-//            pw.flush();
-//            client.shutdownOutput();// 关闭输出流
+//        new Thread( () -> {
+//            try {
+//                // 1.创建 socket 指定服务器地址和端
+//                Socket client = new Socket("127.0.0.1", 8001);
+//                // 2.客户端向服务器发送登录信息
+//                OutputStream os = client.getOutputStream();// 字节输出流
+//                PrintWriter pw = new PrintWriter(new OutputStreamWriter(os,"UTF-8"),true);
+//                pw.write("start");
+//                pw.flush();
+//                client.shutdownOutput();// 关闭输出流
 //
-//            // 3. 获取输入流
-//            InputStream is = client.getInputStream();
-//            InputStreamReader isr = new InputStreamReader(is,"UTF-8");
-//            BufferedReader br = new BufferedReader(isr);
-//            String info = null;
-//            while ((info = br.readLine()) != null) {
-//                System.out.println("服务器发来消息说：" + info);
+//                // 3. 获取输入流
+//                InputStream is = client.getInputStream();
+//                InputStreamReader isr = new InputStreamReader(is,"UTF-8");
+//                BufferedReader br = new BufferedReader(isr);
+//                String info = null;
+//                while ((info = br.readLine()) != null) {
+//                    System.out.println("服务器发来消息说：" + info);
+//                }
+//
+//                // 3.关闭其他资源
+//                pw.close();
+//                os.close();
+//                client.close();
+//            } catch (UnknownHostException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
 //            }
-//
-//            // 3.关闭其他资源
-//            pw.close();
-//            os.close();
-//            client.close();
-//        } catch (UnknownHostException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
+//        });
 
         refreshRobotInfoTimer.patrolTime = System.currentTimeMillis();
-      //模拟机器人收到客户端通知，开始每3秒上报一次数据到数据库
+        //模拟机器人收到客户端通知，开始每3秒上报一次数据到数据库
         new Thread( () -> {
             for (double j = 0; j < 100; j++){
                 double i = j + Math.random() * 100;
